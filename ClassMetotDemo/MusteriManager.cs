e507ca12db7ab108c6fab804217dce0969b3c300
UnaL <UnaL@TOSHIBA>
2021-02-01 03:57:24 +0200

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    { 
+    
+        public void Listele(Musteri a)
+        {
+            Console.WriteLine("-------------------Müşteri-------------------------");
+            Console.WriteLine("Müşterinin adı soyadı :  " +a.Adi + " " + a.Soyadi);
+            Console.WriteLine("Müşterinin Yaşı       :  " + a.MusteriYasi);
+            Console.WriteLine("Mşterinin Mesleği     :  " + a.Meslek);
+            
+        }
+        public void Ekle(string ad, string soyad, int yas, string meslek)
+        {
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("*"+" "+ad+" "+soyad+" müşteri listesine başarıyla eklendi.");
+            Console.WriteLine("Müşterinin adı soyadı :  " + ad + " " + soyad);
+            Console.WriteLine("Müşterinin Yaşı       :  " + yas);
+            Console.WriteLine("Mşterinin Mesleği     :  " + meslek);
+            
+        }
+        public void Sil(Musteri b)
+        {
+            string onay;
+            Console.WriteLine(b.Adi+" "+b.Soyadi+" kişisini müşteri listesinden silmek istiyor musunuz? (evet/hayır): ");
+            onay = Console.ReadLine();
+            if (onay == "evet" )
+            {
+                Console.WriteLine("*" + " " + b.Adi + " " + b.Soyadi + ", müşteri listesinden çıkarıldı.");
+            }
+            else
+            {
+                Console.WriteLine("Ana ekrana döndürülüyorsunuz. Lütfen bekleyin.");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.Adi ="Ünal";
+            musteri.Soyadi = "Mutlu";
+            musteri.MusteriYasi = 30;
+            musteri.Meslek = "Öğretmen";
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Ru";
+            musteri1.Soyadi = "Mutlu";
+            musteri1.MusteriYasi = 26;
+            musteri1.Meslek = "Ev hanımı";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Alperen";
+            musteri2.Soyadi = "Mutlu";
+            musteri2.MusteriYasi = 1;
+            musteri2.Meslek = "Çalışmıyor";
+
+
+            Musteri[] musteriler = new Musteri[] { musteri, musteri1, musteri2};
+            MusteriManager manager = new MusteriManager();
+            
+            foreach (var item in musteriler)
+            {
+                manager.Listele(item);
+            }
+            manager.Ekle("Emrah ", "Mutlu", 34, "Teknisyen");
+            manager.Sil(musteri1);
+        }
+    }
+}
